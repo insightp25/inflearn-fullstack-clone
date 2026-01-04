7ae4980b0a56779e2f1bb99d91cb2d7fe5192bd6
JO <71416000+insightp25@users.noreply.github.com>
2026-01-04 15:30:50 +0900

    exception test

--- backend/src/test/java/clone/inflearn/backend/domain/account/AccountServiceTest.java
+++ backend/src/test/java/clone/inflearn/backend/domain/account/AccountServiceTest.java
@@ -1,18 +1,24 @@
 package clone.inflearn.backend.domain.account;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Fail.fail;
 
 import clone.inflearn.backend.domain.account.enums.AccountRole;
 import clone.inflearn.backend.domain.account.model.Account;
 import clone.inflearn.backend.domain.account.port.AccountRepository;
+import jakarta.transaction.Transactional;
 import java.util.Set;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @SpringBootTest
+@Transactional
 public class AccountServiceTest {
 
     @Autowired
@@ -41,5 +47,24 @@ public class AccountServiceTest {
 
         // then
         assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+    // 예외를 test하는 다양한 방법들
+    @Test
+    public void findByUsernameFail() {
+        String username = "random@email.com";
+
+        // 예외 test 방법1
+        assertThatThrownBy(() -> accountService.loadUserByUsername(username))
+            .isInstanceOf(UsernameNotFoundException.class)
+            .hasMessageContaining(username);
+
+        // 예외 test 방법2
+        try {
+            accountService.loadUserByUsername(username);
+            fail("supposed to be failed");
+        } catch (UsernameNotFoundException e) {
+            assertThat(e.getMessage()).containsSequence(username);
+        }
     }
 }
