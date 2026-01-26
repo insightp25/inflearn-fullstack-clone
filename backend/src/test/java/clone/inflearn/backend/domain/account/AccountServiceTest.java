29b108ccd6c5a614d6cc59af7b290bf614683903
JO <71416000+insightp25@users.noreply.github.com>
2026-01-26 23:13:39 +0900

    spring security oauth2 kakao login basic impl

--- backend/src/test/java/clone/inflearn/backend/domain/account/AccountServiceTest.java
+++ backend/src/test/java/clone/inflearn/backend/domain/account/AccountServiceTest.java
@@ -9,7 +9,6 @@ import clone.inflearn.backend.domain.account.model.Account;
 import clone.inflearn.backend.domain.account.port.AccountRepository;
 import jakarta.transaction.Transactional;
 import java.util.Set;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
